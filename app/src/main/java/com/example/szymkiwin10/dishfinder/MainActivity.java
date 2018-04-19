4ae10b547fadda918dbd44b068532abf42de8fd8
KarolinaH <paczeg@gmail.com>
2018-04-19 14:56:12 +0200

    before settings

--- app/src/main/java/com/example/szymkiwin10/dishfinder/MainActivity.java
+++ app/src/main/java/com/example/szymkiwin10/dishfinder/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button intro_but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+         intro_but = (Button) findViewById(R.id.button);
+         intro_but.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Toast.makeText(getApplicationContext(),"Hello there!",Toast.LENGTH_LONG).show();
+             }
+         });
             }
         });
     }
