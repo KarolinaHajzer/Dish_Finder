05b619441e2f54c9cdbc8037a767df64ff6024d5
KarolinaH <paczeg@gmail.com>
2018-06-02 12:21:01 +0200

    before firebase

--- app/src/main/java/com/example/szymkiwin10/dishfinder/FourthActivity.java
+++ app/src/main/java/com/example/szymkiwin10/dishfinder/FourthActivity.java
@@ -1,26 +1,24 @@
 package com.example.szymkiwin10.dishfinder;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class ThirdActivityVegan extends AppCompatActivity {
-    Button recipe_but;
+public class FourthActivity extends AppCompatActivity {
+    Button try_again_but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third_vegan);
+        setContentView(R.layout.activity_fourth);
 
-        recipe_but = findViewById(R.id.button7);
-        recipe_but.setOnClickListener(new View.OnClickListener() {
+        try_again_but = findViewById(R.id.button8);
+        try_again_but.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(ThirdActivityVegan.this, FourthActivity.class);
+                Intent myIntent = new Intent(FourthActivity.this, SecondActivity.class);
                 startActivity(myIntent);
-
 
             }
         });
--- app/src/main/java/com/example/szymkiwin10/dishfinder/ThirdActivityCarni.java
+++ app/src/main/java/com/example/szymkiwin10/dishfinder/ThirdActivityCarni.java
@@ -1,13 +1,28 @@
 package com.example.szymkiwin10.dishfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivityCarni extends AppCompatActivity {
-
+    Button recipe_but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_carni);
+
+        recipe_but = findViewById(R.id.button5);
+        recipe_but.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ThirdActivityCarni.this, FourthActivity.class);
+                startActivity(myIntent);
+
+
+            }
+        });
     }
 }
